2287fcdd44e5805c8cf2bd7da11d8013c897735a
Michael Burke <mburke@izotope.com>
2016-05-05 13:25:33 -0400

    Do first two bonuses for RPS.

--- lesson_2/rock_paper_scissors.rb
+++ lesson_2/rock_paper_scissors.rb
@@ -1,23 +1,29 @@
 # rock_paper_scissors.rb
 
-VALID_CHOICES = %w(rock paper scissors)
-
-def test_method
-  prompt('test message')
-end
+VALID_CHOICES_HASH = {'r' => 'rock', 'p' => 'paper', 'sc' => 'scissors', 'l' => 'lizard', 'sp' => 'spock'}
+OPTIONS = %w(rock paper scissors lizard spock)
 
 def prompt(message)
   puts "=> #{message}"
 end
 
+def win?(first, second)
+  (first == 'rock' && second == 'scissors') ||
+    (first == 'rock' && second == 'lizard') ||
+    (first == 'paper' && second == 'rock') ||
+    (first == 'paper' && second == 'spock') ||
+    (first == 'scissors' && second == 'paper') ||
+    (first == 'scissors' && second == 'lizard') ||
+    (first == 'lizard' && second == 'spock') ||
+    (first == 'lizard' && second == 'paper') ||
+    (first == 'spock' && second == 'rock') ||
+    (first == 'spock' && second == 'scissors')
+end
+
 def display_results(player, computer)
-  if (player == 'rock' && computer == 'scissors') ||
-     (player == 'paper' && computer == 'rock') ||
-     (player == 'scissors' && computer == 'paper')
+  if win?(player, computer)
     prompt("You won!")
-  elsif (player == 'rock' && computer == 'paper') ||
-        (player == 'paper' && computer == 'scissors') ||
-        (player == 'scissors' && computer == 'rock')
+  elsif win?(computer, player)
     prompt("Computer won!")
   else
     prompt("It's a tie!")
@@ -27,17 +33,26 @@ end
 loop do
   choice = ''
   loop do
-    prompt("Choose one: #{VALID_CHOICES.join(', ')}")
-    choice = gets.chomp
+    message_string = <<-MSG
+      Choose one:
+            r for Rock
+            p for Paper
+            sc for Scissors
+            l for Lizard
+            sp for Spock
+    MSG
+    prompt(message_string)
+    choice = gets.chomp.downcase
 
-    if VALID_CHOICES.include?(choice)
+    if VALID_CHOICES_HASH.include?(choice)
+      choice = VALID_CHOICES_HASH[choice]
       break
     else
       prompt("That's not a valid choice.")
     end
   end
 
-  computer_choice = VALID_CHOICES.sample
+  computer_choice = OPTIONS.sample
 
   puts("You chose: #{choice}; Computer chose: #{computer_choice}")
 
