7cacdc8c4bea0b67df5e135a0929d4e9b37b79f4
Michael Burke <mike.d.burke@gmail.com>
2016-05-16 22:43:09 -0400

    Get through most of twenty one game.

--- lesson_4/twenty_one.rb
+++ lesson_4/twenty_one.rb
@@ -1,13 +1,3 @@
-# 1. Initialize deck
-# 2. Deal cards to player and dealer
-# 3. Player turn: hit or stay
-#   - repeat until bust or "stay"
-# 4. If player bust, dealer wins.
-# 5. Dealer turn: hit or stay
-#   - repeat until total >= 17
-# 6. If dealer bust, player wins.
-# 7. Compare cards and declare winner.
-
 require 'pry'
 
 SUITS = ['H', 'S', 'C', 'D'].freeze
@@ -28,13 +18,13 @@ def total(cards)
 
   sum = 0
   values.each do |value|
-    if value == "A"
-      sum += 11
-    elsif value.to_i == 0 # J, Q, K
-      sum += 10
-    else
-      sum += value.to_i
-    end
+    sum += if value == "A"
+             11
+           elsif value.to_i == 0 # J, Q, K
+             10
+           else
+             value.to_i
+           end
   end
 
   # correct for Aces
@@ -49,15 +39,15 @@ def deal_cards(deck)
   new_hand = deck.shift(4)
   player_hand = [new_hand[0], new_hand[2]]
   dealer_hand = [new_hand[1], new_hand[3]]
-  return player_hand, dealer_hand
+  [player_hand, dealer_hand]
 end
 
-def display_hands(player_hand, dealer_hand)
-  prompt "Dealer has: #{dealer_hand[0][1]} and unknown card"
-  prompt "You have: #{player_hand[0][1]} and #{player_hand[1][1]}"
+def display_initial_hands(player_hand, dealer_hand)
+  prompt "Dealer has: #{dealer_hand[0]} and unknown card"
+  prompt "You have: #{player_hand[0]} and #{player_hand[1]}"
 end
 
-def hit_or_stay
+def player_choice
   loop do
     prompt "Hit or stay? (h or s)"
     answer = gets.chomp
@@ -71,13 +61,116 @@ def hit_or_stay
   end
 end
 
-loop do
-  current_deck = initialize_deck
-
-  player_hand, dealer_hand = deal_cards(current_deck)
-
-  display_hands(player_hand, dealer_hand)
-
-  p player_hand
-  p total(player_hand)
+def busted?(cards)
+  total(cards) > 21
 end
+
+def hits!(hand, deck)
+  hand << deck.shift
+end
+
+def detect_result(player_cards, dealer_cards)
+  player_total = total(player_cards)
+  dealer_total = total(dealer_cards)
+
+  if player_total > 21
+    :player_busted
+  elsif dealer_total > 21
+    :dealer_busted
+  elsif player_total > dealer_total
+    :player_wins
+  elsif dealer_total > player_total
+    :dealer_wins
+  else
+    :tie
+  end
+end
+
+def display_result(player_cards, dealer_cards)
+  result = detect_result(player_cards, dealer_cards)
+
+  case result
+  when :player_busted
+    prompt "You busted! Dealer wins."
+  when :dealer_busted
+    prompt "Dealer busted! You win!"
+  when :player_wins
+    prompt "You win!"
+  when :dealer_wins
+    prompt "Dealer won :("
+  when :tie
+    prompt "Push!"
+  end
+end
+
+def play_again?
+  loop do
+    prompt "Play again? (y or n)"
+    answer = gets.chomp
+    if answer.casecmp('y') == 0
+      return 'y'
+    elsif answer.casecmp('n') == 0
+      return 'n'
+    else
+      prompt "Please type y or n only."
+    end
+  end
+end
+
+loop do # Single Game Loop
+  current_deck = initialize_deck
+  player_hand, dealer_hand = deal_cards(current_deck)
+  loop do # Player Turn Loop
+    display_initial_hands(player_hand, dealer_hand)
+    prompt "Your total is: #{total(player_hand)}"
+    hit_or_stay = player_choice
+
+    if hit_or_stay == 'hit'
+      player_hand = hits!(player_hand, current_deck)
+      prompt "You chose to hit!"
+      prompt "Your cards are now: #{player_hand}"
+      prompt "Your total is now: #{total(player_hand)}"
+    end
+    break if busted?(player_hand) || hit_or_stay == 'stay'
+  end
+
+  if busted?(player_hand)
+    display_result(player_hand, dealer_hand)
+    if play_again? == 'y'
+      next
+    else
+      break
+    end
+  end
+
+  prompt "Dealer's turn now."
+
+  loop do # Dealer Turn Loop
+    break if busted?(dealer_hand) || total(dealer_hand) >= 17
+
+    prompt "Dealer hits!"
+    dealer_hand << current_deck.shift
+    prompt "Dealer's cards are now: #{dealer_hand}"
+  end
+
+  if busted?(dealer_hand)
+    display_result(player_hand, dealer_hand)
+    if play_again? == 'y'
+      next
+    else
+      break
+    end
+  end
+
+  # both player and dealer stays - compare cards!
+  puts "=============="
+  prompt "Dealer has #{dealer_hand}, for a total of: #{total(dealer_hand)}"
+  prompt "Player has #{player_hand}, for a total of: #{total(player_hand)}"
+  puts "=============="
+
+  display_result(dealer_hand, player_hand)
+
+  break unless play_again? == 'y'
+end
+
+prompt "Thanks for playing! Bye!"
